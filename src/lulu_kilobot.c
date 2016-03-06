66aa074e24ed250ef5df902a0f95f5c847f1e325
Andrei Florea <andrei91ro@gmail.com>
2016-03-07 00:52:32 +0200

    constructed basic application structure

---
+++ src/lulu_kilobot.c
@@ -0,0 +1,23 @@
+/**
+ * @file lulu_kilobot.c
+ * @brief Lulu P colony simulator extension that allows controlling a Kilobot robot by using a P colony based controller
+ * In this file we implement all methods that allow a complete Kilobot application to be built.
+ * @author Andrei G. Florea
+ * @author Catalin Buiu
+ * @date 2016-03-06
+ */
+
+#include "lulu_kilobot.h"
+#include "debug.h"
+
+void loop() {
+
+}
+
+void setup() {
+
+}
+
+int main() {
+    return 0;
+}
---
+++ src/lulu_kilobot.h
@@ -0,0 +1,109 @@
+// vim:filetype=c
+/**
+ * @file lulu_kilobot.h
+ * @brief Lulu P colony simulator extension that allows controlling a Kilobot robot by using a P colony based controller
+ * In this header we declare the structure and functions of a basic Kilobot controller that is based on P colonies and implements a bidirectional symbolic object - numeric value conversion.
+ * This application uses the Kilombo simulator and also the kilolib library for interfacing with both a simulated and real kilobot.
+ * @author Andrei G. Florea
+ * @author Catalin Buiu
+ * @date 2016-03-06
+ */
+
+#ifndef LULU_KILOBOT_H
+#define LULU_KILOBOT_H
+
+#include <kilombo.h> //for access to sensors, motors (includes kilolib.h)
+#include "lulu.h"
+
+/**
+ * @brief Define motion types
+ */
+typedef enum {
+    MOTION_STOP,
+    MOTION_FORWARD,
+    MOTION_LEFT,
+    MOTION_RIGHT
+} motion_t;
+
+typedef enum {
+    COLOR_RED = RGB(3, 0, 0),
+    COLOR_GREEN = RGB(0, 3, 0),
+    COLOR_BLUE = RGB(0, 0, 3)
+} led_color_t;
+
+typedef struct {
+    Pcolony_t pcol;
+    uint8_t light,
+            light_prev;
+    motion_t current_motion_state;
+
+} USERDATA;
+
+//TODO params need tunning
+#define PARAM_LIGHT_THRESHOLD 20
+#define PARAM_DISTANCE_THRESHOLD 55
+
+/**
+ * @brief Process raw_state info received from sensors and populate the input module agents with significant objects
+ *
+ * @param mydata USERDATA structure that contains all relevant information about this Kilobot
+ */
+void procInputModule(USERDATA *mydata);
+
+/**
+ * @brief Process the objects present in the output module agents and transform them into commands that are executed by the Kilobot
+ *
+ * @param mydata USERDATA structure that contains all relevant information about this Kilobot
+ */
+void procOutputModule(USERDATA *mydata);
+
+
+/**
+ * @brief Set the motion direction
+ *
+ * @param dir_new The new motion direction (DIR_{STOP, FORWARD, LEFT, RIGHT})
+ */
+void set_motion(motion_t dir_new);
+
+/**
+ * @brief Function called after receiving a new message
+ *
+ * @param _msg Pointer to the new message
+ * @param d Structure that allows to determine the distance from the sender
+ */
+void message_rx(message_t* _msg, distance_measurement_t* d);
+
+/**
+ * @brief Function called when preparing a new message
+ *
+ * @return Pointer to the new message
+ */
+message_t* message_tx();
+
+/**
+ * @brief Function called after the message has been succesfully sent
+ */
+void message_tx_success();
+
+/**
+ * @brief Function called in a continuos loop during the runtine of the program
+ */
+void loop();
+
+/**
+ * @brief Function called only once at startup, before entering the continuos loop
+ */
+void setup();
+
+#ifdef SIMULATOR
+/* provide a text string for the simulator status bar about this bot */
+static char botinfo_buffer[10000];
+/**
+ * @brief Prepares a status bar message for the simulator
+ *
+ * @return The status bar message
+ */
+char *cb_botinfo(void);
+#endif
+
+#endif
